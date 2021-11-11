6506d198f3ee6c8461f1b78dae87142540184006
vitorsoaresf <vitorsoaresf@gmail.com>
2021-11-11 13:29:07 -0300

    feat: add styled-component GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import GlobalStyle from "./styles/global";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App"></div>;
+    </>
+  );
 }
 
 export default App;
